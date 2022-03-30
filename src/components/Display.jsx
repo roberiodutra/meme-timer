6760bfba98daab034315bce36b5dd12108f21ba1
Robério Almeida <dev.roberio@gmail.com>
2022-03-29 21:06:54 -0300

    fix: style css align

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
 import PlusTime from "./PlusTime";
 import LessTime from "./LessTime";
+import Timer from "./Timer";
 
 class Display extends Component {
-  Timer = (number) => {
-    let value = number.toString();
-    if (value.length < 2) {
-      value = "0" + value;
-    }
-    return value;
-  };
-
   render() {
     const { minutes, seconds, plusTime, lessTime, isHidden } = this.props;
     return (
       <div className="display">
         <PlusTime plusTime={plusTime} isHidden={isHidden} />
-        <span className="timer">
-          {this.Timer(minutes)}:{this.Timer(seconds)}
-        </span>
+        <Timer seconds={seconds} minutes={minutes} />
         <LessTime lessTime={lessTime} isHidden={isHidden} />
       </div>
     );
