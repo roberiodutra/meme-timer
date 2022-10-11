436e0af34de60aca49dcbec6395052ba7e2b88be
Robério Dutra <92614697+roberiodutra@users.noreply.github.com>
2022-10-11 15:46:46 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
       <GitHubIcon style={{ fontSize: "10px" }} />{" "}
       <span>
         Made by{" "}
-        <a href="https://github.com/Roberio-Almeida/meme-timer">
-          @Roberio-Almeida
+        <a href="https://github.com/roberiodutra">
+          @Roberio-Dutra
         </a>
       </span>
     </footer>
